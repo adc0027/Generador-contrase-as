8af2533a8367ba6aa3fb0e9f0d205fc224e99c0d
adc0027 <alvarodonaire101@gmail.com>
2025-11-20 17:23:03 +0100

    Enhance contrasenas.py with comments
    Added comments for better code understanding.

--- contrasenas.py
+++ contrasenas.py
@@ -1,10 +1,14 @@
 import string
 import random
 
+#Input de la longitud de la contraseña
 longitud = int(input("Ingrese la longitud de la contraseña: "))
 
+#Tipos de caracteres a incluir en la contraseña
 caracteres = string.ascii_letters + string.digits + string.punctuation
 
+#Generación de la contraseña
 contrasena = "".join(random.choice(caracteres) for i in range(longitud))
 
+#Mostrar la contraseña generada
 print("La contraseña es :" + contrasena)
